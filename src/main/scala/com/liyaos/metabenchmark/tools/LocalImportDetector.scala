4a32d05fbd22eb5c05c6fc83d124966629c8d718
Yao Li <hnkfliyao@gmail.com>
2015-10-24 11:22:48 +0800

    Parse Scala imports.

--- src/main/scala/com/liyaos/metabenchmark/tools/LocalImportDetector.scala
+++ src/main/scala/com/liyaos/metabenchmark/tools/LocalImportDetector.scala
@@ -7,6 +7,7 @@ package com.liyaos.metabenchmark.tools
 import java.nio.file.{Paths, Path}
 import java.io.File
 import scala.io.Source
+import fastparse.all._
 
 case class NoViableDetectorException(file: File) extends Exception
 
@@ -18,6 +19,8 @@ object LocalImportDetector {
       LocalRepoDirDetector(Paths.get(file.getAbsolutePath))
     else if (file.getName.toLowerCase.endsWith(".java"))
       LocalRepoJavaImportDetector(Paths.get(file.getAbsolutePath))
+    else if (file.getName.toLowerCase.endsWith(".scala"))
+      LocalRepoScalaImportDetector(Paths.get(file.getAbsolutePath))
     else throw NoViableDetectorException(file)
   }
 }
@@ -44,16 +47,48 @@ case class LocalRepoDirDetector(path: Path) extends LocalImportDetector(path) {
 }
 
 case class LocalRepoJavaImportDetector(path: Path) extends LocalImportDetector(path) {
-
   override def imports: Set[String] = {
     Source.fromFile(path.toFile).getLines().filter(_.trim.startsWith("import ")).map { line =>
       line.trim
     }.map { line =>
-      line.substring(line.indexOfSlice("import ") + 7, line.size - 1)
+      line.substring(line.indexOfSlice("import ") + 7, line.size - 1).trim
     }.map { line =>
       if (line.contains("static "))
-        line.trim.substring(line.indexOfSlice("static") + 7)
-      else line.trim
+        line.substring(line.indexOfSlice("static") + 7)
+      else line
     }.toSet
   }
+}
+
+case class LocalRepoScalaImportDetector(path: Path) extends LocalImportDetector(path) {
+  override def imports: Set[String] = {
+    Source.fromFile(path.toFile).getLines().map { line =>
+      ScalaImportParser.importExpr.parse(line.trim) match {
+        case Result.Success(im, _) => Some(im)
+        case Result.Failure(_, _) => None
+      }
+    }.flatten.flatten
+  }.toSet
+}
+
+trait ScalaBasics {
+  val word: P[Unit]= P(CharIn('a' to 'z' ) ~ CharIn(('a' to 'z') ++ ('0' to '9')).rep(0))
+}
+
+object ScalaImportParser extends ScalaBasics {
+  val White = fastparse.WhitespaceApi.Wrapper{
+    import fastparse.all._
+    NoTrace(" ".rep)
+  }
+  import White._
+  val libExpr: P[Seq[String]] = P((packageExpr.rep(1).! ~ objExpr).map { x =>
+    val pre = x._1
+    x._2.map(y => pre + y)
+  })
+  val packageExpr: P[String] = P((word ~ ".").!)
+  val nameExpr: P[String] = P(word.! ~ "=>" ~ word | word.!)
+  val namesExpr: P[Seq[String]] = P("{" ~ nameExpr.rep(1, sep = ",") ~ "}")
+  val objExpr: P[Seq[String]] = P(word.!.map(Seq(_)) | namesExpr)
+
+  val importExpr: P[Seq[String]] = P("import" ~! libExpr )
 }
