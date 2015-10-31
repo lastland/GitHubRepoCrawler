739268774b54d07429eaa6b259ba9b6854ed1f6d
Luca Salucci <salucl@usi.ch>
2015-10-31 13:34:15 +0100

    Added different type of Actors in MainArguments

--- src/main/scala/com/liyaos/metabenchmark/MainArguments.scala
+++ src/main/scala/com/liyaos/metabenchmark/MainArguments.scala
@@ -6,7 +6,10 @@ package com.liyaos.metabenchmark
 object MainArguments {
 
   val outputFolder: String = "./actors/";
-  val classToFilter: Array[String] =  Array("akka.actor");
+  val classToFilter: Array[String] =  Array("akka.actor.Actor", "akka.actor.UntypedActor",
+    "scala.actors.Actor", "org.jetlang.fibers.Fiber", "fj.control.parallel.Actor",
+    "groovyx.gpars.actor.Actor", "edu.rice.hj.api.HjActor", "fi.jumi.actors.Actors",
+    "net.liftweb.actor.LiftActor","scalaz.consurrent.Actor");
 //  val classToFilter: Array[String] =  Array("java.util.concurrent.Executor",
 //    "java.util.concurrent.ExecutorService", "java.util.concurrent.AbstractExecutorService",
 //    "java.util.concurrent.ThreadPoolExecutor", "java.util.concurrent.ForkJoinPool");
