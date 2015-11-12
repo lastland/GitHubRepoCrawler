8786aac0cfa0998935a4a0c9e50fe8a3773a696e
Luca Salucci <salucl@usi.ch>
2015-11-12 11:54:51 +0100

    Created update branch

--- findExecutionTime.sh
+++ findExecutionTime.sh
@@ -2,34 +2,20 @@
 
 if [ $# -lt 2 ]
 then
-<<<<<<< HEAD
-	echo "findExecutionTime.sh <folder> <pattern>"
-	exit -1
-=======
     echo "findExecutionTime.sh <folder> <pattern>"
     exit -1
->>>>>>> 6c4665d0d0b9881598c24b213cb4c732478ebbe3
 fi
 
-find $1 -name results*`hostname` > result-files.txt
+find $1 -name results*@* > result-files.txt
 
 rm file-with-test-result-list.txt
+touch file-with-test-result-list.txt
 while read line; do
-<<<<<<< HEAD
-     	#echo "Inspecting: $line"
-	grep $2 $line
-	if [[ $? -eq 0 ]]
-	then
-		echo $line >> file-with-test-result-list.txt
-	fi
-done < result-files.txt
-=======
     #echo "Inspecting: $line"
-    grep $2 $line
+    cat $line
+		grep $2 $line
     if [[ $? -eq 0 ]]
     then
         echo $line >> file-with-test-result-list.txt
     fi
 done < result-files.txt
-
->>>>>>> 6c4665d0d0b9881598c24b213cb4c732478ebbe3
