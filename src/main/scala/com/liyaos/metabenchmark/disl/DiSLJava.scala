3575de3aca7bba05e5eab0707adf5b08e3c6c464
zhengyudi <jhengyudi@gmail.com>
2015-11-13 16:30:56 +0100

    bug fix: remove prefix of profiler.jar

--- src/main/scala/com/liyaos/metabenchmark/disl/DiSLJava.scala
+++ src/main/scala/com/liyaos/metabenchmark/disl/DiSLJava.scala
@@ -14,7 +14,7 @@ object DiSLJava extends ScriptInstaller {
   private val libSuffix = if (System.getProperty("os.name") == "Mac OS X") "jnilib" else "so"
   private val agent = s"${DiSLConfig.dislHome}/lib/libdislagent.$libSuffix"
   private val bypass = s"${DiSLConfig.dislHome}/lib/disl-bypass.jar"
-  private val bootClass = s"-Xbootclasspath/a:$bypass:${DiSLConfig.installationDir}/${DiSLConfig.instJar}"
+  private val bootClass = s"-Xbootclasspath/a:$bypass:${DiSLConfig.instJar}"
   val fakeJavaHome = DiSLConfig.installationDir + "/FakeHome"
   private val java = fakeJavaHome + "/bin/java"
 
